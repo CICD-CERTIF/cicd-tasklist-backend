50ffc271e3ae8572d1e2d07be1f5402a80c21c3b
Djoumessi2004 <wilfridndongmo@gmail.com>
2026-07-02 12:24:15 +0200

    test(fix) change configs

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from "vitest/config";
 
-// On force Prisma à utiliser un fichier SQLite local pendant les tests
-process.env.DATABASE_URL = 'file:./test-e2e.db';
-
 export default defineConfig({
   test: {
     globals: true,
     environment: "node",
-    
-    // CORRECTION ICI : On cible directement TOUT ce qui est dans __tests__ sans exception
-    include: ["src/__tests__/**/*.{test,spec,}.ts", "src/__tests__/**/*.ts"],
-    
+    include: ["src/__tests__/**/*.test.ts"],
     testTimeout: 15000,
     coverage: {
       provider: "v8",
@@ -20,9 +14,6 @@ export default defineConfig({
       exclude: [
         "src/__tests__/**",
         "src/server.ts",
-        "src/app.ts",          
-        "src/lib/prisma.ts",   
-        "src/routes/**",       
         "**/*.config.ts",
       ],
     },
